157bcb3e17970926a65dffd116f5fa40264d76a3
Sarvjeetjha <jha.sarvjeet1@gmail.com>
2024-07-07 11:58:56 +0530

    program to check positive or negative

---
+++ Basics of python/Assighnment/1_simpleIfElse.py
@@ -0,0 +1,3 @@
+#Write a program that asks the user to input a number and prints whether the number is positive.
+num=int(input("enter a number"))
+print('positive number') if num>=0 else print("less than one")
