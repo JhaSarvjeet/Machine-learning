22a81469b99ea1ac5312e98b63695a7078599203
Sarvjeetjha <jha.sarvjeet1@gmail.com>
2024-07-07 12:39:45 +0530

    loop to add number from 1 to 10 using while loop

--- Basics of python/Assignment/Loops/1_numberFrom1_to_10.py
+++ Basics of python/Assignment/Loops/1_numberFrom1_to_10.py
@@ -1,3 +1,11 @@
-for num in range(1,11):
-    print(num,end=" ")
+#using for loop
+# for num in range(1,11):
+#     print(num,end=" ")
+
+
+#using while loop
+num=1
+while(num<=10):
+    print(num,end =" ")
+    num=num+1
     
